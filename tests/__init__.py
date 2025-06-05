14f9446470efd0839f9a915c523cb14ae079a463
Simone Nogara <simone@intarmour.com>
2025-06-05 22:23:27 +0200

    Update __init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,7 @@
 # File: tests/test_aws.py
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import unittest
 from providers.aws import AWSProvider
 
